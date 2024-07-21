d2a0dde027918bbbab27fc18147b66701f34b000
moreno-m2698 <moreno.m2698@gmail.com>
2024-07-21 18:20:50 -0400

    update Graph.tsx: fixed schema in componentDidMount

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -27,7 +27,7 @@ class Graph extends Component<IProps, {}> {
       price_def: 'float',
       ratio: 'float',
       timestamp: 'date',
-      upper_bond: 'float',
+      upper_bound: 'float',
       lower_bound: 'float',
       trigger_alert: 'float'
     };
